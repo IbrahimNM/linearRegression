95ff8b6e91884e18710af297c56cf7656390c40f
Ibrahim <almohaimeed.6@wright.edu>
2019-04-29 11:59:31 +0300

    Add class blue-print

---
+++ simpleLinearRegression/LinearRegression.py
@@ -0,0 +1,26 @@
+
+
+class LinearRegression(object):
+    
+    def __init__(self):
+        pass
+    
+    def getLinearRegressionFunction(self):
+        ''' y(x) = a + bx '''
+        return 0
+
+    def getSlope(self):
+        ''' b = r * (Sy/Sx) '''
+        return 0
+    
+    def getYintercept(self):
+        ''' a = y' - bx' '''
+        return 0
+    
+    def getPearsonCorrelationCoefficient(self):
+        ''' r = sum((x-x')(y-y')) / sqrt(sum(x-x')^2 * sum(y-y')^2) '''
+        return 0
+    
+    def getStandardDeviation(self):
+        ''' Sx = sqrt(sum(x-x')^2 / n-2) '''
+        return 0
