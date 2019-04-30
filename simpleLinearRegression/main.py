cafbd40c204813bb79b95c068a54b7e688b0ea5a
Ibrahim <almohaimeed.6@wright.edu>
2019-04-30 13:16:46 +0300

    Fix formatting issue

--- simpleLinearRegression/main.py
+++ simpleLinearRegression/main.py
@@ -3,7 +3,7 @@ from LinearRegression import LinearRegression
 x = [1400, 1600, 1700, 1875, 1100, 1550, 2350, 2450, 1425, 1700]
 y = [245,312,279,308,199,219,405,324,319,255]
 current = LinearRegression(x,y)
-""" print current
+print current
 print 'Summation of x: ', current.getSummation(x)
 print 'Summation of y: ', current.getSummation(y)
 print 'Number of digits in x:', current.getCount(x)
@@ -15,7 +15,6 @@ print 'yDiff: (y - y\')', current.getDiff(y)
 print 'StandardDiff of x: ', current.getStandardDeviation(x)
 print 'StandardDiff of y: ', current.getStandardDeviation(y)
 print 'Product of (x - x\')*(y - y\'): ', current.getProduct(current.getDiff(x), current.getDiff(y))
-    """
 print 'Y: ', current.getYintercept()
 print 'Slope: ', current.getSlope() 
 func = current.getLinearRegressionFunction()
