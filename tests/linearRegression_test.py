8ae476fbea66058faf7b0fa7f8a2f1c79468505a
Ibrahim <almohaimeed.6@wright.edu>
2019-04-30 14:02:32 +0300

    Add pytest.parametrize method for testing

--- tests/linearRegression_test.py
+++ tests/linearRegression_test.py
@@ -6,7 +6,7 @@ from simpleLinearRegression.LinearRegression import LinearRegression
 SCOPE = "function"
 
 global current
-current = LinearRegression([0],[0])
+current = LinearRegression([0], [0])
 
 
 @pytest.fixture(scope=SCOPE)
@@ -14,6 +14,29 @@ def shared_instance():
     global current
     yield current
 
+
+@pytest.mark.parametrize(
+    'number, word', [
+        (1, '1'),
+        (3, 'Fizz'),
+        (5, 'Buzz'),
+        (10, 'Buzz'),
+        (15, 'FizzBuzz'),
+        (16, '16')
+    ]
+)
+
+def test_para(number, word):
+    assert number is not None
+
+
 def test_getSlope(shared_instance):
     assert True
 
+
+def test_getX(shared_instance):
+    assert shared_instance.getX() == [0]
+
+
+def test_getY(shared_instance):
+    assert shared_instance.getY() == [0]
