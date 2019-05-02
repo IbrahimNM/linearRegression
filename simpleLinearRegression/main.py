3ce8b3e27f84d25c9e319491d3837876535419db
Ibrahim <almohaimeed.6@wright.edu>
2019-05-02 14:31:37 +0300

    Test linear sample, removed vscode conf file, and add vscode to gitignore

--- simpleLinearRegression/main.py
+++ simpleLinearRegression/main.py
@@ -1,8 +1,10 @@
 from LinearRegression import LinearRegression
 
-x = [1400, 1600, 1700, 1875, 1100, 1550, 2350, 2450, 1425, 1700]
-y = [245,312,279,308,199,219,405,324,319,255]
-current = LinearRegression(x,y)
+x = [1,2,3,4,5,6,7,8]
+y = [11,22,33,44,55,66,77,88]
+
+
+current = LinearRegression(x, y)
 print current
 print 'Summation of x: ', current.getSummation(x)
 print 'Summation of y: ', current.getSummation(y)
@@ -14,9 +16,10 @@ print 'xDiff (x - x\'): ', current.getDiff(x)
 print 'yDiff: (y - y\')', current.getDiff(y)
 print 'StandardDiff of x: ', current.getStandardDeviation(x)
 print 'StandardDiff of y: ', current.getStandardDeviation(y)
-print 'Product of (x - x\')*(y - y\'): ', current.getProduct(current.getDiff(x), current.getDiff(y))
+print 'Product of (x - x\')*(y - y\'): ', current.getProduct(
+    current.getDiff(x), current.getDiff(y))
 print 'Y: ', current.getYintercept()
-print 'Slope: ', current.getSlope() 
+print 'Slope: ', current.getSlope()
 func = current.getLinearRegressionFunction()
 print 'Function: ', current.getLinearRegressionFunction()
 print 'f(15): ', func(15)
